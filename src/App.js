1c3133785d8a122ca53bc12ca4a1c57a1471cf6c
Hrushi4 <hrushikeshchavan257@gmail.com>
2021-10-12 00:00:26 +0530

    Web push notifications

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{useEffect}from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Hero from './components/Hero';
 import { GlobalStyle } from './globalStyles';
@@ -6,9 +6,18 @@ import { productData, productDataTwo } from './components/Products/data';
 import Products from './components/Products';
 import Feature from './components/Feature';
 import Footer from './components/Footer';
+import OneSignal from 'react-onesignal';
+
+
 
 function App() {
+  useEffect(() => {
+    OneSignal.init({
+      appId: "6a562109-7349-4306-bef1-eb3728d7ede5"
+    });
+  }, []);
   return (
+  
     <Router>
       <GlobalStyle />
       <Hero />
